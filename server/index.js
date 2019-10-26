c439e3041f6aeb14fe4da9ea108729f38806a92a
Patrick Thibodeau <patrick.thibodeau@gmail.com>
2019-10-26 16:20:22 -0500

    project complete

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const massive = require("massive");
 const { CONNECTION_STRING, SESSION_SECRET } = process.env;
 const authCtrl = require("./controllers/authController");
 const treasureCtrl = require("./controllers/treasureController");
+const auth = require("./middleware/authMiddleware");
 
 const app = express();
 
@@ -27,5 +28,15 @@ app.use(express.json());
 app.post("/auth/register", authCtrl.register);
 app.post("/auth/login", authCtrl.login);
 app.get("/auth/logout", authCtrl.logout);
+
+app.get("/api/treasure/dragon", treasureCtrl.dragonTreasure);
+app.get("/api/treasure/user", auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post("/api/treasure/user", auth.usersOnly, treasureCtrl.addUserTreasure);
+app.get(
+  "/api/treasure/all",
+  auth.usersOnly,
+  auth.adminsOnly,
+  treasureCtrl.getAllTreasure
+);
 
 app.listen(PORT, () => console.log(`app is listening on port: ${PORT}`));
